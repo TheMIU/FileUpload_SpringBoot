0c84eef51908e0b787c29c70d05f82bce88b7c03
Kasun Miuranga <kasunmiuranga2000@gmail.com>
2023-10-18 18:37:50 +0530

    uploads folder creation added.

--- Backend/src/main/java/lk/ijse/springboot/fileupload/controller/FileUploadController.java
+++ Backend/src/main/java/lk/ijse/springboot/fileupload/controller/FileUploadController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.PostConstruct;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -22,15 +23,50 @@ import java.util.List;
 @RestController
 @RequestMapping("/file-upload")
 public class FileUploadController {
-    private final String fileUploadPath = "F:\\\\GitHub MY\\\\FileUpload_SpringBoot\\\\uploads\\\\";
+    private String uploadsFolderPath;
+
+
+    public FileUploadController() {
+        // File upload path check
+        checkUploadFolderCreated();
+    }
+
+    public void checkUploadFolderCreated() {
+        // Get the absolute path to the project's base directory
+        String basePath = new File("").getAbsolutePath();
+
+        // Define the path for the "uploads" folder by appending "uploads" to the base path
+        uploadsFolderPath = basePath + File.separator + "uploads" + File.separator;
+        System.out.println("uploadsFolderPath : " + uploadsFolderPath);
+
+        // Create a File object to represent the 'uploads' folder based on the specified path
+        File uploadsFolder = new File(uploadsFolderPath);
+
+        // Check if the 'uploads' folder exists
+        if (!uploadsFolder.exists()) {
+            // if not create one
+            boolean folderCreated = uploadsFolder.mkdirs();
+
+            // Check if the folder was successfully created
+            if (folderCreated) {
+                System.out.println("The 'uploads' folder was successfully created.");
+            } else {
+                System.out.println("Failed to create the 'uploads' folder.");
+            }
+        } else {
+            System.out.println("The 'uploads' folder already exists.");
+        }
+    }
 
     @PostMapping("/upload")
     public ResponseEntity<String> uploadFile(@RequestParam MultipartFile file) throws IOException {
+        checkUploadFolderCreated();
+
         try {
             String originalFilename = file.getOriginalFilename();
-            String filePath = fileUploadPath + originalFilename;
+            String filePath = uploadsFolderPath + originalFilename;
             System.out.println(filePath);
-            file.transferTo(new File(fileUploadPath + file.getOriginalFilename()));// Save the file with the specified path
+            file.transferTo(new File(uploadsFolderPath + file.getOriginalFilename()));// Save the file with the specified path
             return ResponseEntity.ok(filePath); // Return the file path in the response
         } catch (IOException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Upload failed: " + e.getMessage());
@@ -40,9 +76,11 @@ public class FileUploadController {
 
     @GetMapping("/upload/{filename:.+}")
     public ResponseEntity<Resource> getImage(@PathVariable String filename) {
+        checkUploadFolderCreated();
+
         System.out.println("invoked get");
         // Load the image file and return it as a response
-        Path imagePath = Paths.get(fileUploadPath, filename);
+        Path imagePath = Paths.get(uploadsFolderPath, filename);
         try {
             if (Files.exists(imagePath) && Files.isReadable(imagePath)) {
                 Resource resource = new UrlResource(imagePath.toUri());
@@ -59,13 +97,15 @@ public class FileUploadController {
 
     @GetMapping("/list-images")
     public ResponseEntity<List<ImageInfo>> listImages() {
+        checkUploadFolderCreated();
+
         System.out.println("invoked");
 
         List<ImageInfo> images = new ArrayList<>();
 
         try {
             // List all files in the uploads directory
-            Files.list(Paths.get(fileUploadPath))
+            Files.list(Paths.get(uploadsFolderPath))
                     .filter(Files::isRegularFile) // Filter out directories
                     .forEach(file -> {
                         String filePath = file.toAbsolutePath().toString();
@@ -82,3 +122,5 @@ public class FileUploadController {
     }
 
 }
+
+
